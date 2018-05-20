ec4531050be4021ea263c07322321bac0677c73b
Ruslan Yandiev <yandiev_97@mail.ru>
2018-05-20 17:54:51 +0300

    Map fixed

--- js/main.js
+++ js/main.js
@@ -4,7 +4,203 @@ new Vue({
     currentTab: 0,
     tabs: ["Мытищи", "Москва"]
   },
-  created: function() {
-    
+  mounted: function() {
+    const element = document.querySelector("#map");
+    // COORDINATES FOR PLACES
+    var mytishchi = {lat: 55.918320, lng: 37.768983};
+    var moscow = {lat: 55.681355, lng: 37.590211};
+    // MAP OPTIONS
+    var options = {
+      zoom: 15,
+      center: mytishchi,
+      disableDefaultUI: true,
+      styles: [
+          {
+              "featureType": "all",
+              "elementType": "labels.text.fill",
+              "stylers": [
+                  {
+                      "saturation": 36
+                  },
+                  {
+                      "color": "#000000"
+                  },
+                  {
+                      "lightness": 40
+                  }
+              ]
+          },
+          {
+              "featureType": "all",
+              "elementType": "labels.text.stroke",
+              "stylers": [
+                  {
+                      "visibility": "on"
+                  },
+                  {
+                      "color": "#000000"
+                  },
+                  {
+                      "lightness": 16
+                  }
+              ]
+          },
+          {
+              "featureType": "all",
+              "elementType": "labels.icon",
+              "stylers": [
+                  {
+                      "visibility": "off"
+                  }
+              ]
+          },
+          {
+              "featureType": "administrative",
+              "elementType": "geometry.fill",
+              "stylers": [
+                  {
+                      "color": "#000000"
+                  },
+                  {
+                      "lightness": 20
+                  }
+              ]
+          },
+          {
+              "featureType": "administrative",
+              "elementType": "geometry.stroke",
+              "stylers": [
+                  {
+                      "color": "#000000"
+                  },
+                  {
+                      "lightness": 17
+                  },
+                  {
+                      "weight": 1.2
+                  }
+              ]
+          },
+          {
+              "featureType": "landscape",
+              "elementType": "geometry",
+              "stylers": [
+                  {
+                      "color": "#000000"
+                  },
+                  {
+                      "lightness": 20
+                  }
+              ]
+          },
+          {
+              "featureType": "poi",
+              "elementType": "geometry",
+              "stylers": [
+                  {
+                      "color": "#000000"
+                  },
+                  {
+                      "lightness": 21
+                  }
+              ]
+          },
+          {
+              "featureType": "road.highway",
+              "elementType": "geometry.fill",
+              "stylers": [
+                  {
+                      "color": "#000000"
+                  },
+                  {
+                      "lightness": 17
+                  }
+              ]
+          },
+          {
+              "featureType": "road.highway",
+              "elementType": "geometry.stroke",
+              "stylers": [
+                  {
+                      "color": "#000000"
+                  },
+                  {
+                      "lightness": 29
+                  },
+                  {
+                      "weight": 0.2
+                  }
+              ]
+          },
+          {
+              "featureType": "road.arterial",
+              "elementType": "geometry",
+              "stylers": [
+                  {
+                      "color": "#000000"
+                  },
+                  {
+                      "lightness": 18
+                  }
+              ]
+          },
+          {
+              "featureType": "road.local",
+              "elementType": "geometry",
+              "stylers": [
+                  {
+                      "color": "#000000"
+                  },
+                  {
+                      "lightness": 16
+                  }
+              ]
+          },
+          {
+              "featureType": "transit",
+              "elementType": "geometry",
+              "stylers": [
+                  {
+                      "color": "#000000"
+                  },
+                  {
+                      "lightness": 19
+                  }
+              ]
+          },
+          {
+              "featureType": "water",
+              "elementType": "geometry",
+              "stylers": [
+                  {
+                      "color": "#000000"
+                  },
+                  {
+                      "lightness": 17
+                  }
+              ]
+          }
+      ]
+    };
+    var myMap = new google.maps.Map(element, options);
+    // CHANGE MAP CENTER POSITION ON TAB CLICK
+    google.maps.event.addDomListener(document.querySelector("#mapTab:nth-child(1)"), "click", function() {
+      myMap.setCenter(new google.maps.LatLng(mytishchi.lat, mytishchi.lng));
+    });
+    google.maps.event.addDomListener(document.querySelector("#mapTab:nth-child(2)"), "click", function() {
+      myMap.setCenter(new google.maps.LatLng(moscow.lat, moscow.lng));
+    });
+    // SET MARKERS
+    var markers = [mytishchi, moscow];
+    for (var i = 0; i < markers.length; i++) {
+      createMarker(markers[i]);
+    }
+    function createMarker(coords) {
+      var marker = new google.maps.Marker({
+        position: coords,
+        map: myMap,
+        icon: "img/marker.png "
+      });
+    }      
   }
 })
